3e1eb8b514159041f4d5710805de565dd66a5432
Kenvin Emiewo <kenvin.emiewo@workforcegroup.com>
2019-01-20 18:51:32 +0100

    restructre home component to use children components like Placeholder, Analytics, Clicks and GeneralInfo

---
+++ src/actions/backend.actions.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import {
+    API_BASE_URL,
+    TOKEN
+} from '../config';
+import { 
+    BEFORE_RESULT,
+    SHORTEN_URL,
+    ERROR 
+} from './types';
+
+
+export const persistResource = () => {
+    //write a service to accept & save the persisted resource
+}
+
+export const viewAllResources = () => {
+    //write a service to view all the persisted resources
+}
---
+++ src/actions/generalinfo.actions.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import {
+    API_BASE_URL,
+    TOKEN
+} from '../config';
+import { 
+    BEFORE_RESULT,
+    SHORTEN_URL,
+    ERROR 
+} from './types';
+
+
