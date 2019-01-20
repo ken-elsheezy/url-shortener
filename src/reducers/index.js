f9047a8ea849b47e5056317809a608c7a96b3892
Kenvin Emiewo <kenvin.emiewo@workforcegroup.com>
2019-01-20 21:55:49 +0100

    complete url shortening flow on home page

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 import homeReducer from './home.reducer';
+import clicksReducer from './clicks.reducer';
+import generalInfoReducer from './generalinfo.reducer';
 
 
 export default combineReducers({
-    home: homeReducer
+    home: homeReducer,
+    clicks: clicksReducer,
+    general: generalInfoReducer
 });
