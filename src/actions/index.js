6099e68e2884886387d57b86afb218b5894af34e
Kenvin Emiewo <kenvin.emiewo@workforcegroup.com>
2019-01-20 16:21:14 +0100

    implement request for short url from external api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,53 @@
-import { BEFORE_RESULT } from './types';
+import axios from 'axios';
+import {
+    API_BASE_URL,
+    TOKEN
+} from '../config';
+import { 
+    BEFORE_RESULT,
+    SHORTEN_URL,
+    ERROR 
+} from './types';
 
-export const beforeResult = () => {
-    return {type:BEFORE_RESULT, payload:null};
+
+export const beforeFetchResult = () => {
+    return {
+        type:BEFORE_RESULT, 
+        payload:null
+    }
+}
+
+export const getShortenedUrl = (longURL) => {
+
+    return async function(dispatch){
+        try{
+
+            let headers = {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + TOKEN 
+            }  
+
+            const response = await  axios.post(
+                API_BASE_URL+'shorten', 
+                {long_url : longURL}, 
+                {headers: headers}
+            );
+
+            console.log(response);
+
+            dispatch({type:SHORTEN_URL, payload: response.data});
+
+        }catch(e){
+            dispatch({type:ERROR, payload: e});
+        }
+    }
+
+}
+
+export const persistResource = () => {
+    //write a service to accept & save the persisted resource
+}
+
+export const viewAllResources = () => {
+    //write a service to view all the persisted resources
 }
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,3 @@
 export const BEFORE_RESULT = 'BEFORE_RESULT';
+export const SHORTEN_URL = 'SHORTEN_URL';
+export const ERROR = 'ERROR';
