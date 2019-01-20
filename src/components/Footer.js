8850dce47acd80b3589e9e2fd732cc1ceb4506bc
Kenvin Emiewo <kenvin.emiewo@workforcegroup.com>
2019-01-20 01:50:00 +0100

    add boostrap for responsive layout, create components for layout including Header, Footer, Loader

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const Footer = () => {
+    return(
+        <footer>
+            <p>&copy; 2019 URL Shortener & Analytics.</p>
+        </footer>
+    );
+}
+
+export default Footer;
