05ad3492a64939f1e3a894c474b90f156398c5dd
Kenvin Emiewo <kenvin.emiewo@workforcegroup.com>
2019-01-21 02:44:05 +0100

    workflow complete

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+const Card = (props) => {
+
+    return (
+
+        <div className="col-md-12">
+            <p style={{padding:'10%', backgroundColor:'#fff',marginBottom:10, textAlign:'center'}}>
+                <a href={"https://"+props.item.value} target="_blank">
+                    <img src="/extras/image/check.png" style={{width:25}} />  {props.item.id}  || {props.item.value}
+                </a> 
+            </p>      
+        </div>    
+          
+    );
+  
+}
+
+export default Card;
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import homeReducer from './home.reducer';
 import clicksReducer from './clicks.reducer';
 import generalInfoReducer from './generalinfo.reducer';
+import listReducer from './list.reducer';
 
 
 export default combineReducers({
     home: homeReducer,
     clicks: clicksReducer,
-    general: generalInfoReducer
+    general: generalInfoReducer,
+    list: listReducer
 });
